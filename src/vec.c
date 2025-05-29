8b57325fa82778f810a62d1e5c634f8797279470
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-05-29 16:40:30 +0800

    Get Clang-21 to build

--- src/vec.c
+++ src/vec.c
@@ -10,18 +10,3 @@ _Thread_local struct vec_a vec_array_tmp = { 0 };
 #endif
 
 extern inline ssize_t vec_capacity_auto(ssize_t x);
-
-
-#ifdef __clang__
-struct qsort_frw_data { noplate_qsort_cmp_func_t fun; void* data; };
-static int qsort_block_forward(const void* a, const void* b, void* _d)
-{
-	struct qsort_frw_data* data = _d;
-	return data->fun(a, b, data->data);
-}
-extern void noplate_qsort(void* ptr, size_t N, size_t si, noplate_qsort_cmp_func_t cmp, void* data)
-{
-	qsort_r(ptr, N, si, qsort_block_forward, &(struct qsort_frw_data){ cmp, data });
-}
-#endif
-
